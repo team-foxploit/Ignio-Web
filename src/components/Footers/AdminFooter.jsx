40b1cd49ff96b80bb2106fd9c11ebd064a9906cf
luk3Sky <ganindudananja@gmail.com>
2019-07-01 17:09:38 +0530

    Update AdminFooter.jsx

--- src/components/Footers/AdminFooter.jsx
+++ src/components/Footers/AdminFooter.jsx
@@ -11,14 +11,14 @@ class Footer extends React.Component {
         <Row className="align-items-center justify-content-xl-between">
           <Col xl="6">
             <div className="copyright text-center text-xl-left text-muted">
-              © 2018{" "}
+              © 2019{" "}
               <a
                 className="font-weight-bold ml-1"
-                href="https://www.creative-tim.com?ref=adr-admin-footer"
+                href="http://www.foxploit.com"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                Creative Tim
+                Foxploit
               </a>
             </div>
           </Col>
@@ -27,17 +27,17 @@ class Footer extends React.Component {
             <Nav className="nav-footer justify-content-center justify-content-xl-end">
               <NavItem>
                 <NavLink
-                  href="https://www.creative-tim.com?ref=adr-admin-footer"
+                  href="http://www.foxploit.com"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  Creative Tim
+                  Foxploit
                 </NavLink>
               </NavItem>
 
               <NavItem>
                 <NavLink
-                  href="https://www.creative-tim.com/presentation?ref=adr-admin-footer"
+                  href="http://www.foxploit.com/about-us"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
@@ -47,7 +47,7 @@ class Footer extends React.Component {
 
               <NavItem>
                 <NavLink
-                  href="http://blog.creative-tim.com?ref=adr-admin-footer"
+                  href="http://www.foxploit.com/blog"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
@@ -57,11 +57,11 @@ class Footer extends React.Component {
 
               <NavItem>
                 <NavLink
-                  href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-admin-footer"
+                  href="https://github.com/team-foxploit"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  MIT License
+                  GitHub
                 </NavLink>
               </NavItem>
             </Nav>
