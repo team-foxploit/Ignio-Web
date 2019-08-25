5f4c49f6b4cb6187d467940efd97e4e887c78a01
luk3Sky <ganindudananja@gmail.com>
2019-08-26 02:47:01 +0530

    SettingsHeader component added

---
+++ src/components/Headers/SettingsHeader.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+
+// reactstrap components
+import { Badge, Button, Card, CardBody, CardText, CardTitle, Container, Row, Col } from "reactstrap";
+
+class SettingsHeader extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="header bg-gradient-warning pb-8 pt-5 pt-md-8">
+          <Container fluid>
+            <div className="header-body">
+              {/* Card stats */}
+              <Row>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Incidents
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            0
+                          </span>
+                        </div>
+                        <Col className="col-auto">
+                          <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                            <i className="fas fa-fire" />
+                          </div>
+                        </Col>
+                      </Row>
+                      <p className="mt-3 mb-0 text-muted text-sm">
+                        <span className="text-success mr-2">
+                          <i className="fa fa-arrow-up" /> 0%
+                        </span>{" "}
+                        <span className="text-nowrap">Since last month</span>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardText tag="h5" className="text-uppercase text-info">
+                            <Button color="primary" type="button" className="px-2">
+                              <i className=" ni ni-fat-add" />
+                              <span>Add Devices</span>
+                            </Button>
+                          </CardText>
+                        </div>
+                        <Col className="col-auto">
+                          <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+                          <i className=" ni ni-box-2" />
+                          </div>
+                        </Col>
+                      </Row>
+                      <p className="mt-3 mb-0 text-muted text-sm">
+                        <span className="text-success mr-2">
+                          <i className="fas fa-arrow-up" /> 100%
+                        </span>{" "}
+                        <span className="text-nowrap">Since last week</span>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardText tag="h5" className="text-uppercase text-info">
+                            <Button color="danger" type="button" className="px-2">
+                              <i className="ni ni-active-40"></i>
+                              <span>Test Now!</span>
+                            </Button>
+                          </CardText>
+                        </div>
+                        <Col className="col-auto">
+                            <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                                <i className="ni ni-settings" />
+                            </div>
+                        </Col>
+                      </Row>
+                      <p className="mt-3 mb-0 text-muted text-sm">
+                        <span className="text-warning mr-2">
+                          <i className="fas fa-arrow-down" /> 1.10%
+                        </span>{" "}
+                        <span className="text-nowrap">Last: February 2019</span>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col lg="6" xl="3">
+                  <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <div className="col">
+                          <CardTitle
+                            tag="h5"
+                            className="text-uppercase text-muted mb-0"
+                          >
+                            Status
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            100%
+                          </span>
+                        </div>
+                        <Col className="col-auto">
+                          <div className="icon icon-shape bg-info text-white rounded-circle shadow">
+                            <i className="fas fa-percent" />
+                          </div>
+                        </Col>
+                      </Row>
+                      <p className="mt-3 mb-0 text-muted text-sm">
+                        <span className="text-success mr-2">
+                          <i className="fas fa-arrow-up" /> 100%
+                        </span>{" "}
+                        <span className="text-nowrap">Performance sync</span>
+                      </p>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+      </>
+    );
+  }
+}
+
+export default SettingsHeader;
